7553c74bc5eb9abb7d90272d7503a9f3c73ae52c
Moinul Haq <moinulhaq.se@gmail.com>
2024-07-25 05:54:13 -0400

    chore: Update dependencies and add 3D rendering feature

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
+import { Canvas } from "@react-three/fiber"
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl">App</h1>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'tailwindcss.css'
+import 'tailwindcss/tailwind.css'
 import App from 'components/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
