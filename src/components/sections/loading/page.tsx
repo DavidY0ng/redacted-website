fd6caf5b4f7ba935852a904e3102341e53fe8f96
DavidYong <yongcheeho@gmail.com>
2024-07-30 14:18:33 +0800

    feat: added loading bar

--- src/components/sections/loading/page.tsx
+++ src/components/sections/loading/page.tsx
@@ -2,6 +2,10 @@ import * as React from "react"
 import { Progress } from "../../ui/progress"
 import { AnimatedText } from "@/components/animation/staggerText";
 import style from './style.module.css'
+import slantedTrapezium from 'assets/img/sections/loading/slanted trapezium.png'
+import square from 'assets/img/sections/loading/square.png'
+import { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 function LoadingProgress() {
   const [progress, setProgress] = React.useState(0)
@@ -44,11 +48,50 @@ function LoadingFont({ progress}:{progress:number}) {
     return (
         <div>
             <div className="text-lg text-white flex">
-                    <AnimatedText text={letters} repeatDelay={5000} className="animate-pulse"></AnimatedText>
+                    <AnimatedText text={letters} repeatDelay={3000} className="animate-pulse"></AnimatedText>
                 <span className="ml-2">{Math.round(progress)}%</span>
             </div>
         </div>
     )
+}
+
+
+
+
+function LoadingTrapezium() {
+  const controls1 = useAnimation();
+  const controls2 = useAnimation();
+  const controls3 = useAnimation();
+  const controls4 = useAnimation();
+  const controls5 = useAnimation();
+
+  const startBlinking = async (controls) => {
+    while (true) {
+      await controls.start({ opacity: 0, transition: { duration: 0.2 } });
+      await controls.start({ opacity: 1, transition: { duration: 0.2 } });
+      await new Promise((resolve) => setTimeout(resolve, Math.random() * 2000));
+    }
+  };
+
+  useEffect(() => {
+    startBlinking(controls1);
+    startBlinking(controls2);
+    startBlinking(controls3);
+    startBlinking(controls4);
+    startBlinking(controls5);
+  }, [controls1, controls2, controls3, controls4, controls5]);
+
+  return (
+    <div className="flex items-end">
+      <motion.img src={slantedTrapezium} className="h-5 w-4" animate={controls1} />
+      <motion.img src={slantedTrapezium} className="h-5 w-4" animate={controls2} />
+      <motion.img src={slantedTrapezium} className="h-5 w-4" animate={controls3} />
+      <motion.img src={slantedTrapezium} className="h-5 w-4" animate={controls4} />
+      <div className="flex items-end pl-1">
+        <motion.img src={square} className="h-2 w-2" animate={controls5} />
+      </div>
+    </div>
+  );
 }
 
 export default function LoadingPage() {
@@ -57,6 +100,7 @@ export default function LoadingPage() {
             <div className="absolute bottom-[20%] left-[10%]">
                 <div className="flex items-center space-x-5">
                     <LoadingProgress />
+                    <LoadingTrapezium />
                 </div>
             </div>
         </div>
