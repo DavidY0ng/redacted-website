0f67fb54e8361e739fab08542f4618fd913b7c08
DavidYong <yongcheeho@gmail.com>
2024-09-10 13:22:48 +0800

    chores: desktop view for about page

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -8,15 +8,27 @@ import { LeftFrame, RightFrame } from '@/components/frame/Frame'
 
 export default function About() {
   return (
-    <div className="relative h-screen bg-[#b31e1e]">
-      <div className="absolute flex h-full flex-col justify-center gap-3 px-10">
-        <img src={loading} className="w-1/2"></img>
-        <img src={re}></img>
-        <img src={generationz}></img>
-        <img src={nft}></img>
-        <div className="flex flex-col gap-3 mt-10 w-3/4">
-          <img src={dot} className='w-[6%]'></img>
-          <img src={description} className='w-full'></img>
+    <div className="relative h-screen   bg-[#b31e1e]">
+      <div className="flex justify-center">
+        <div className="absolute flex h-full max-w-[1200px] flex-col justify-center gap-3 px-10">
+          <img src={loading} className="block w-1/2 md:hidden"></img>
+          <img src={re} className="w-full max-w-[80%] md:max-w-[50%]"></img>
+          <img src={generationz} className=''></img>
+          <img src={nft} className="block md:hidden "></img>
+          {/* desktop */}
+          <div className="md:flex md:gap-10">
+            <div className="hidden w-full items-start justify-end md:flex">
+              <img src={loading} className=" w-1/2 pt-2"></img>
+            </div>
+            <div className="mt-10 flex w-3/4 flex-col gap-3 md:mt-0 md:w-full md:items-start">
+              <img src={nft} className="hidden w-full md:flex"></img>
+              <img src={dot} className="flex w-[6%] md:hidden"></img>
+              <div className="flex w-2/3 justify-start md:mt-10">
+                <img src={dot} className="hidden w-[5%] md:flex"></img>
+              </div>
+              <img src={description} className="w-full"></img>
+            </div>
+          </div>
         </div>
       </div>
 
