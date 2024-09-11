e0573a1e9f22bf50747b9baf8579e91e252d6533
DavidYong <yongcheeho@gmail.com>
2024-09-11 15:28:46 +0800

    chores: change intro image to font

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import Intro from './sections/intro/Intro'
 import About from './sections/about/About'
 import NFTCollection from './sections/nft-collection/Nft-collection'
 import Footer from './footer/Footer'
+import '../global.css'
 
 function App() {
   return (
--- src/components/sections/intro/Intro.tsx
+++ src/components/sections/intro/Intro.tsx
@@ -18,7 +18,14 @@ function Character() {
 function Font() {
   return (
     <div className="relative w-full max-w-[500px] md:max-w-[600px]">
-      <img src={font}></img>
+      <p className='leading-9 lg:leading-tight mb-5 md:mb-10 font-euroblack text-white text-[28px] md:text-[30px] lg:text-[45px]'>
+          STEP INTO THE FANTASY WORLD OF <span className='text-red-700'>RE -</span> GENZ
+      </p>
+      <p className='font-eurostile text-white/60 text-[15px] md:text-[18px] lg:text-[22px]'>
+        RE-GENZ stands for regenerative generations, emphasizing sustainability, community, and innovation.
+        This project leverages the power of NFTS to create a unique ecosystem where digital assets provide value
+        both online and offline.
+      </p>
     </div>
   )
 }
