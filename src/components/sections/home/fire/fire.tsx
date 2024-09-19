18932009add4836af4fd7240d58a29be7fbd4ee9
DavidYong <yongcheeho@gmail.com>
2024-09-19 15:50:59 +0800

    chores: change fire to gif

--- src/components/sections/home/fire/fire.tsx
+++ src/components/sections/home/fire/fire.tsx
@@ -1,32 +1,12 @@
-import fire from '@/assets/img/sections/fire'
+import fire from '@/assets/img/sections/fire/fire.gif'
 
 export default function FireBg() {
   return (
-    <div className="relative mx-auto flex h-[700px] max-w-[1400px] justify-center overflow-hidden bg-[#b31e1e] ">
+    <div className="relative mx-auto flex h-[700px] justify-center overflow-hidden bg-[#b31e1e] ">
       <img
-        src={fire.fire1}
-        loading="lazy"
-        className="absolute bottom-0 left-0 z-[100] hidden h-[700px] translate-x-[-300px]  translate-y-[150px] opacity-80 md:block lg:translate-x-0"
-      ></img>
-      <img
-        src={fire.fire2}
-        loading="lazy"
-        className="absolute bottom-0 left-0 z-[100] h-[800px] translate-x-[-130px] translate-y-[200px] opacity-80 md:left-[10%] lg:left-[20%]"
-      ></img>
-      <img
-        src={fire.fire3}
-        loading="lazy"
-        className="absolute bottom-0 z-[100] hidden h-[700px] translate-x-[50px] translate-y-[150px] opacity-80 lg:block"
-      ></img>
-      <img
-        src={fire.fire3}
-        loading="lazy"
-        className="absolute bottom-0 right-0 z-[100] h-[700px] translate-x-[130px] translate-y-[150px] opacity-80 md:right-[10%] lg:right-[15%]"
-      ></img>
-      <img
-        src={fire.fire4}
-        loading="lazy"
-        className="absolute bottom-0 right-0 z-[100] hidden h-[700px] translate-x-[300px] translate-y-[150px] opacity-80 md:block lg:translate-x-[140px]"
+        src={fire}
+        loading="eager"
+        className="absolute h-[1000px] md:h-[1600px] w-full bottom-0 z-[100]"
       ></img>
     </div>
   )
--- src/components/sections/outro/Outro.tsx
+++ src/components/sections/outro/Outro.tsx
@@ -3,12 +3,12 @@ import squad_cropped from '@/assets/img/outro/squad-cropped.webp'
 
 export default function Outro() {
   return (
-    <div className="relative h-[30vh]  lg:h-[70vh] bg-white">
+    <div className="relative h-[30vh]  bg-white lg:h-[70vh]">
       <div className="relative mx-auto flex h-full max-w-[1600px] items-center justify-center">
-        <img src={re_bg} className="absolute h-[25vh] lg:h-[55vh] px-5"></img>
+        <img src={re_bg} className="absolute h-[25vh] px-5 lg:h-[55vh]"></img>
         <img
           src={squad_cropped}
-          className="absolute h-[30vh] lg:h-[70vh] translate-y-[4%]"
+          className="absolute h-[30vh] translate-y-[4%] lg:h-[70vh]"
         ></img>
       </div>
     </div>
