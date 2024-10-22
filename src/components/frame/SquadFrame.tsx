9115b324487f26e2af27618604eb842f682f69b0
DavidYong <yongcheeho@gmail.com>
2024-10-22 11:15:52 +0800

    chore: update mobile view for squad

---
+++ src/components/frame/SquadFrame.tsx
@@ -0,0 +1,81 @@
+import images from 'assets/img/sections/landing/frame-elements/index'
+import icons from 'assets/img/sections/landing/icons/index'
+
+export function LeftFrame() {
+  return (
+    <div className="relative flex h-[150vh] md:h-screen flex-col items-center justify-center gap-10">
+      <img
+        src={images.leftFrame1}
+        loading="eager"
+        className="absolute top-0 mt-2 h-3"
+      ></img>
+      <img
+        src={images.leftFrame2}
+        loading="eager"
+        className="absolute top-20 h-[35%]"
+      ></img>
+      <img src={images.leftFrame3} loading="eager" className="h-3"></img>
+      <img
+        src={images.leftFrame4}
+        loading="eager"
+        className="absolute bottom-20 h-[35%]"
+      ></img>
+      <img
+        src={images.leftFrame5}
+        loading="eager"
+        className="absolute bottom-0 mb-2 h-3"
+      ></img>
+    </div>
+  )
+}
+
+export function RightFrame() {
+  return (
+    <div className="relative flex h-[150vh] md:h-screen flex-col items-center justify-center gap-10">
+      <img
+        src={images.rightFrame1}
+        loading="eager"
+        className="absolute top-0 mt-2 h-3"
+      ></img>
+      <img
+        src={images.rightFrame2}
+        loading="eager"
+        className="absolute top-20 h-[35%]"
+      ></img>
+      <img src={images.rightFrame3} loading="eager" className="h-3"></img>
+      <img
+        src={images.rightFrame4}
+        loading="eager"
+        className="absolute bottom-20 h-[35%]"
+      ></img>
+      <img
+        src={images.rightFrame5}
+        loading="eager"
+        className="absolute bottom-0 mb-2 h-3"
+      ></img>
+    </div>
+  )
+}
+
+export function Icons() {
+  return (
+    <div className="relative flex items-center justify-center gap-3">
+      <img src={icons.icon1} loading="eager" className="h-[50px]"></img>
+      <img src={icons.icon2} loading="eager" className="h-[50px]"></img>
+      <img src={icons.icon3} loading="eager" className="h-[50px]"></img>
+    </div>
+  )
+}
+
+export function WholeStandardFrame() {
+  return (
+    <>
+      <div className="absolute left-0 h-screen">
+        <LeftFrame />
+      </div>
+      <div className="absolute right-0 h-screen">
+        <RightFrame />
+      </div>
+    </>
+  )
+}
--- src/components/sections/squad/Squad.tsx
+++ src/components/sections/squad/Squad.tsx
@@ -10,7 +10,7 @@ import squad5Mobile from '@/assets/img/sections/squad/mobile/squad_people_5.png'
 import squad5Desktop from '@/assets/img/sections/squad/desktop/squad_people_5.png'
 import squad6Mobile from '@/assets/img/sections/squad/mobile/squad_people_6.png'
 import squad6Desktop from '@/assets/img/sections/squad/desktop/squad_people_6.png'
-import { WholeStandardFrame } from '@/components/frame/Frame'
+import { LeftFrame, RightFrame } from '@/components/frame/SquadFrame'
 // import Carousel from '@/components/carousel/Carousel'
 import {
   Carousel,
@@ -100,10 +100,10 @@ export default function Squad() {
     return () => window.removeEventListener('resize', handleResize)
   }, [])
   return (
-    <div className="relative flex min-h-screen w-full items-center justify-center bg-black">
-      <div className="flex w-full max-w-[1600px] flex-col gap-[100px] px-4 sm:pb-20 md:px-10">
-        <div className="flex size-full justify-center pl-10 md:justify-start">
-          <h1 className="font-euroblack w-[260px] text-left text-4xl font-bold leading-[90px] text-white md:w-[800px] md:text-[80px]">
+    <div className="relative flex h-[150vh] w-full items-center justify-center bg-black md:h-screen">
+      <div className="flex w-full max-w-[1600px] flex-col gap-[120px] px-4 sm:pb-20 md:gap-[100px] md:px-10 ">
+        <div className="md:mt-10 flex size-full justify-center pl:3 md:justify-start md:pl-10 lg:pl-20">
+          <h1 className="font-euroblack text-left w-[320px]  text-[45px] font-bold leading-none text-white md:w-[450px] md:text-[60px] md:leading-[90px] lg:w-[800px] lg:text-[80px]">
             MEET THE <span className="text-red">SQUAD</span>
           </h1>
         </div>
@@ -117,35 +117,98 @@ export default function Squad() {
             gap="16px"
           />
         </div> */}
-        <div className="flex h-full  w-full flex justify-center pl-10">
-          <Carousel
-            orientation={isMobile ? 'vertical' : 'horizontal'}
-            opts={{ align: 'start', loop: true }}
-            className="w-full max-w-[1200px] "
-          >
-            <CarouselContent className="">
-              {squadMembers.map((member) => (
-                <CarouselItem
-                  key={member.id}
-                  className="basis-1/3 md:basis-1/3 lg:basis-1/4 flex justify-center"
-                >
-                  <div className=" size-[85%]">
-                    <img
-                      src={isMobile ? member.mobileImage : member.desktopImage}
-                      alt={`Squad member ${member.id}`}
-                      className=" object-cover"
-                    />
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
+        <div className="size-full justify-center hidden md:flex">
+          <DesktopCarousel />
+        </div>
+        <div className="flex justify-center">
+          <MobileCarousel />
         </div>
       </div>
-      
-      <WholeStandardFrame />
+
+      <div className="absolute left-0 h-[150vh] md:h-screen">
+        <LeftFrame />
+      </div>
+      <div className="absolute right-0 h-[150vh] md:h-screen">
+        <RightFrame />
+      </div>
     </div>
+  )
+}
+
+function DesktopCarousel() {
+  const [isMobile, setIsMobile] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768)
+    handleResize() // Initial check
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  return (
+    <Carousel
+      orientation={isMobile ? 'vertical' : 'horizontal'}
+      opts={{ align: 'start', loop: true }}
+      className="w-full bg-gray-500 md:max-w-[550px] lg:max-w-[800px] xl:max-w-[1100px]"
+    >
+      <CarouselContent className="">
+        {squadMembers.map((member) => (
+          <CarouselItem
+            key={member.id}
+            className="flex basis-1/3 justify-center md:basis-1/3 lg:basis-1/4"
+          >
+            <div className="size-[50%] md:size-[95%] lg:size-[85%]">
+              <img
+                src={isMobile ? member.mobileImage : member.desktopImage}
+                alt={`Squad member ${member.id}`}
+                className=" object-cover"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+function MobileCarousel() {
+  const [isMobile, setIsMobile] = React.useState(false)
+
+  // Handle screen size detection
+  React.useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768)
+    handleResize() // Initial check
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  return (
+    <Carousel
+      opts={{
+        align: 'start'
+      }}
+      orientation="vertical"
+      className="w-full max-w-xs md:hidden block"
+    >
+      <CarouselContent className="-mt-1 h-[580px]">
+        {squadMembers.map((member) => (
+          <CarouselItem
+            key={member.id}
+            className="flex basis-1/3 justify-center md:basis-1/3 lg:basis-1/4"
+          >
+            <div className="size-[50%] md:size-[95%] lg:size-[85%]">
+              <img
+                src={isMobile ? member.mobileImage : member.desktopImage}
+                alt={`Squad member ${member.id}`}
+                className=" object-cover"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   )
 }
